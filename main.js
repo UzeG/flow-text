c9c4ced6bd9730ac7487c62d58491791082c852d
UzeG <uzegcod1ver@gmail.com>
2023-12-29 17:07:30 +0800

    feat: local storage

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let flowText = "Hello World!";
 let flowSpeedRate = 2;
+let sizeRate = 1;
 
 const setElTranslateX = (el, px) => {
     el.style.transform = `translate(${px}px)`;
@@ -7,19 +8,53 @@ const setElTranslateX = (el, px) => {
 
 const resolveUrl = () => {
     const urlParams = new URLSearchParams(window.location.search);
-    return { text: urlParams.get('text'), speed: urlParams.get('speed') };
+    return { text: urlParams.get('text'), speed: Number(urlParams.get('speed')), size: Number(urlParams.get('size')) };
 }
 
+let isFullScreen = false;
+
 window.onload = function () {
-    const { text, speed } = resolveUrl();
-    text && (flowText = text);
-    speed && (flowSpeedRate = speed);
+    window.addEventListener('dblclick', () => {
+        if (isFullScreen) {
+            document.exitFullscreen()
+        } else {
+            document.body.requestFullscreen();
+        }
+        isFullScreen = !isFullScreen;
+    })
+
+    const { text, speed, size } = resolveUrl();
+
+    if (text) {
+        flowText = text;
+        localStorage.setItem('text', flowText);
+    } else {
+        const l_flowText = localStorage.getItem('text');
+        l_flowText ? (flowText = l_flowText) : localStorage.setItem('text', flowText);
+    }
+
+    if (speed) {
+        flowSpeedRate = speed;
+        localStorage.setItem('speed', flowSpeedRate);
+    } else {
+        const l_flowSpeedRate = localStorage.getItem('speed');
+        l_flowSpeedRate ? (flowSpeedRate = l_flowSpeedRate) : localStorage.setItem('speed', flowSpeedRate);
+    }
+
+    if (size) {
+        sizeRate = size;
+        localStorage.setItem('size', sizeRate);
+    } else {
+        const l_sizeRate = localStorage.getItem('size');
+        l_sizeRate ? (sizeRate = l_sizeRate) : localStorage.setItem('size', sizeRate);
+    }
 
     const rootEl = document.getElementById("root");
 
     let flowTextEl1 = document.createElement("div");
     flowTextEl1.textContent = flowText;
     flowTextEl1.classList.add("flow-text");
+    flowTextEl1.style.setProperty('font-size', `${10 * sizeRate}rem`)
     let flowTextEl2 = flowTextEl1.cloneNode(true);
 
     rootEl.appendChild(flowTextEl1);
